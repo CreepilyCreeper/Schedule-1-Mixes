6de02d114273e842801eeb4f9cbcc17c0b43de6b
CreepilyCreeper <leungsamuel929@gmail.com>
2025-04-26 01:40:03 +0800

    add Mix Access Page with JSON data table functionality, compile all json to data.json later

---
+++ json-merger.py
@@ -0,0 +1,19 @@
+import json
+import glob
+
+read_files = glob.glob("*.json")
+output_list = []
+
+for f in read_files:
+    with open(f, "rb") as infile:
+        output_list.append(json.load(infile))
+'''
+all_items = []
+for json_file in output_list:
+    all_items += json_file['items']
+'''
+all_items = output_list
+
+textfile_merged = open('data.json', 'w')
+json.dump({ "items": all_items }, textfile_merged)
+textfile_merged.close()
